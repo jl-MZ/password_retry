17ce54c48996a576e7f379dc68d178eb0aa97f9a
Jlin <jianglin661@gmail.com>
2020-06-02 15:59:12 +0900

    basic version

---
+++ password2.py
@@ -0,0 +1,12 @@
+password = 'b12345'
+i = 3
+while True:
+    pwd = input('请输入密码:')
+    if pwd == password:
+        print('登入成功！')
+        break
+    else:
+        i = i-1
+        print('密码错误!还有',i,'次机会')
+        if i == 0:
+          break
